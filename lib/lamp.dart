07d8cc0f9985fce1b3f047f74159180730778d4c
Nitish Kumar Singh <nitishk72@gmail.com>
2018-05-06 10:01:21 +0530

    Update lamp.dart

--- lib/lamp.dart
+++ lib/lamp.dart
@@ -7,8 +7,5 @@ class Lamp {
   static const MethodChannel _channel = const MethodChannel('github.com/clovisnicolas/flutter_lamp');
   static Future turnOn({double intensity = 1.0}) => _channel.invokeMethod('turnOn', {"intensity" : intensity.clamp(0.01, 1.0)});
   static Future turnOff() => _channel.invokeMethod('turnOff');
-  static Future<bool> get hasLamp async{
-    bool b = await _channel.invokeMethod('hasLamp');
-    return b;
-  }
+  static Future<bool> get hasLamp async => await _channel.invokeMethod('hasLamp')
 }
